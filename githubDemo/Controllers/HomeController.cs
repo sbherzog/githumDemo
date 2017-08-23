9d33d9d0d860d60b53a829aebf0ac183525dd5df
SAMUELHERZOB6AC\samuelherzog <sbh5350@gmail.com>
2017-08-22 22:21:42 -0400

    edit controller

--- githubDemo/Controllers/HomeController.cs
+++ githubDemo/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace githubDemo.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
